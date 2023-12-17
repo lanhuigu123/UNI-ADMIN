a0f7753a7adb09ecc30a6b7c9290e13648ee5556
lanhuigu <2405872445@qq.com>
2023-12-17 14:27:47 +0800

    feat: 新建项目包
    新建项目包

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div></div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue"
-
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld
-  }
+  name: "homeView",
+  components: {},
+  data() {
+    return {}
+  },
+  created() {},
+  mounted() {},
+  methods: {}
 }
 </script>
+<style scoped lang="scss"></style>
